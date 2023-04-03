ecc39de0cf8991c1c2bbd6761fc2787a2709f711
Jonni Le <jole@student.hive.fi>
2023-04-03 19:32:11 +0300

    changed print_error char * to const char *

--- print_error.c
+++ print_error.c
@@ -6,13 +6,13 @@
 /*   By: jole <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:28:32 by jole              #+#    #+#             */
-/*   Updated: 2023/04/03 19:20:01 by jole             ###   ########.fr       */
+/*   Updated: 2023/04/03 19:31:32 by jole             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_error(char *error_message, char *input)
+void	print_error(const char *error_message, char *input)
 {
 	write(2, error_message, ft_strlen(error_message));
 	free(input);
