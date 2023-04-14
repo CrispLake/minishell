84585b65a3f42c048aca47d379b12fd5c81ed3cb
Eemil Majuri <eemilmajuri@gmail.com>
2023-04-14 20:50:46 +0300

    Fixed print error so it adds newline after error

--- src/print_error.c
+++ src/print_error.c
@@ -6,7 +6,7 @@
 /*   By: jole <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:28:32 by jole              #+#    #+#             */
-/*   Updated: 2023/04/07 14:55:36 by jole             ###   ########.fr       */
+/*   Updated: 2023/04/14 20:48:45 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	print_error(const char *error_message, char *input)
 {
 	write(2, error_message, ft_strlen(error_message));
+	write(2, "\n", 1);
 	free(input);
 }
