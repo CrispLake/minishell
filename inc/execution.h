ebcf4cad5dd6918c452b8b943b3e029a34e9170a
Eemil Majuri <eemilmajuri@gmail.com>
2023-04-18 18:24:41 +0300

    Added the struct for fds

--- inc/execution.h
+++ inc/execution.h
@@ -6,8 +6,18 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:46:30 by emajuri           #+#    #+#             */
-/*   Updated: 2023/04/17 18:06:02 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/04/18 18:21:58 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EXECUTION_H
+# define EXECUTION_H
 
+typedef struct s_fd
+{
+	int	fd_in;
+	int	fd_out;
+	int	pipe[2];
+}	t_fd;
+
+#endif
