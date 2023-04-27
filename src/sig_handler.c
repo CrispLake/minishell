467c9a1945a298f96f697196c51fad5d63451fed
Eemil Majuri <eemilmajuri@gmail.com>
2023-04-27 17:40:37 +0300

    Fixed so exit wont be in the middle of text and is on it's own line

--- src/sig_handler.c
+++ src/sig_handler.c
@@ -6,7 +6,7 @@
 /*   By: jole <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 15:21:43 by jole              #+#    #+#             */
-/*   Updated: 2023/04/27 14:29:37 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/04/27 17:38:59 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ void	open_echo_control(struct termios *t)
 
 void	ctrl_d_handler(void)
 {
-	write(1, "\033[1A\033[13C", 9);
 	write(1, "exit\n", 5);
 	exit(1);
 }
